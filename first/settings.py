3c7b2019620838443671dbfbe5e83d67d01a499a
taraxdev <techworks29@gmail.com>
2021-04-15 15:41:40 +0530

    0.2

--- first/settings.py
+++ first/settings.py
@@ -108,3 +108,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'first/static'),
+]
